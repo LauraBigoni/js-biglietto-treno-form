eda3dd5d15241512fc7cb362e839eda996d8caac
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-12 16:22:05 +0100

    Add form

---
+++ js/script.js
@@ -0,0 +1,23 @@
+console.log('JS OK!');
+/* 
+TRACCIA:
+Il programma dovrà mostrare una form da compilare con cui chiedere all'utente 
+il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, 
+secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina 
+(formattato con massimo due decimali, per indicare centesimi sul prezzo).
+
+BONUS:
+replicare anche lo stile estetico fornito dallo screenshot tramite CSS
+
+ATTENZIONE:
+- Controlliamo che tipo di valore viene restituito dai value degli input?
+- Evitiamo per il momento di utilizzare il tag <form> di HTML , 
+poichè potrebbe giocarci qualche scherzetto inaspettato!
+*/
+
+// Recupero gli elementi in pagina
