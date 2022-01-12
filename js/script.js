b674f84d2a86f9622fdef743525873ce96dbc362
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-12 18:52:17 +0100

    add price calc and add delete btn function

--- js/script.js
+++ js/script.js
@@ -26,53 +26,73 @@ const kmElement = document.getElementById('km');
 const ageElement = document.getElementById('age');
 const buttonElement = document.getElementById('generate');
 const buttonDeleteElement = document.getElementById('cancel');
-const placeholderElement = document.getElementById('placeholder');
+const insertNameElement = document.getElementById('insert-name');
+const insertOfferElement = document.getElementById('insert-offer');
+const insertCarrozzaElement = document.getElementById('insert-carrozza');
+const insertCpElement = document.getElementById('insert-cp');
+const insertPriceElement = document.getElementById('insert-price');
 
 // Creo le variabili per il calcolo del costo del biglietto
-const price = 0.21;
-let kmPercorsi = 0;
-let userAge = 0;
-let percentage = 0;
-let finalPrice = 0;
-let discountValue = 0;
-const sum = price * kmPercorsi;
-let message = 'Il costo del tuo biglietto è:';
+
 
 // Faccio funzionare i button
 buttonElement.addEventListener('click', function () {
     const nameValue = nameElement.value;
-    const  kmValue = parseInt(kmElement.value);
+    const kmValue = parseInt(kmElement.value);
     const ageValue = ageElement.value;
+    const price = 0.21;
+    let percentage = 0;
+    let discountValue = 0;
+    let sum = price * kmValue.toFixed(2);
+    let finalPrice = 0;
+
     console.log(nameValue);
     console.log(kmValue);
     console.log(ageValue);
+
     nameElement.value = '';
     kmElement.value = '';
     ageElement.value = '';
-    
+
     if (nameValue) {
-    placeholderElement.innerText = nameValue + kmValue + ageValue;
+    insertNameElement.innerText = nameValue;
+    
+    // Calcolo il prezzo del biglietto e mostro in pagina i risultati
+    if (ageValue === 'Minorenne') {
+    percentage = 20;
+    discountValue = (sum / 100) * percentage;
+    finalPrice = (sum - discountValue).toFixed(2)
+    console.log(finalPrice);
+
+    insertOfferElement.innerText = 'Riduzione Minorenni';
+    insertPriceElement.innerText = `${finalPrice} €`;
+
+    } else if (ageValue === 'Maggiorenne') {
+    console.log(sum);
+
+    insertOfferElement.innerText = 'Biglietto Standard';
+    insertPriceElement.innerText = `${sum} €`;
+
+    } else if (ageValue === 'Over 65') {
+    percentage = 40;
+    discountValue = (sum / 100) * percentage;
+    finalPrice = (sum - discountValue).toFixed(2);
+    console.log(finalPrice);
+
+    insertOfferElement.innerText = 'Riduzione Senior';
+    insertPriceElement.innerText = `${finalPrice} €`;
     }
+}
 });
 
 buttonDeleteElement.addEventListener('click', function () {
     nameElement.value = '';
     kmElement.value = '';
     ageElement.value = '';
+
+    insertNameElement.innerText = '';
+    insertOfferElement.innerText = '';
+    insertCarrozzaElement.innerText = '';
+    insertCpElement.innerText = '';
+    insertPriceElement.innerText = '';
 });
-
-// Calcolo il prezzo del biglietto
-if (userAge < 18) { 
-    percentage = 20;
-    discountValue = (sum / 100) * percentage;
-    console.log(discountValue);
-} else if (userAge >= 65) {
-    percentage = 40;
-    discountValue = (sum / 100) * percentage;
-    console.log(discountValue);
-} else {
-    console.log(sum);
-}
-
-// finalPrice = (sum - discountValue).toFixed(2);
-// console.log(`${message} ${finalPrice} €`);
