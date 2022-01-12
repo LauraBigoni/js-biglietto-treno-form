385d41e2d2ce08f72285dde7a4fb85ef78ec8ea0
Laura Bigoni <laurabigoni.92@gmail.com>
2022-01-12 17:51:54 +0100

    added style

--- js/script.js
+++ js/script.js
@@ -21,3 +21,58 @@ poichè potrebbe giocarci qualche scherzetto inaspettato!
 */
 
 // Recupero gli elementi in pagina
+const nameElement = document.getElementById('name');
+const kmElement = document.getElementById('km');
+const ageElement = document.getElementById('age');
+const buttonElement = document.getElementById('generate');
+const buttonDeleteElement = document.getElementById('cancel');
+const placeholderElement = document.getElementById('placeholder');
+
+// Creo le variabili per il calcolo del costo del biglietto
+const price = 0.21;
+let kmPercorsi = 0;
+let userAge = 0;
+let percentage = 0;
+let finalPrice = 0;
+let discountValue = 0;
+const sum = price * kmPercorsi;
+let message = 'Il costo del tuo biglietto è:';
+
+// Faccio funzionare i button
+buttonElement.addEventListener('click', function () {
+    const nameValue = nameElement.value;
+    const  kmValue = parseInt(kmElement.value);
+    const ageValue = ageElement.value;
+    console.log(nameValue);
+    console.log(kmValue);
+    console.log(ageValue);
+    nameElement.value = '';
+    kmElement.value = '';
+    ageElement.value = '';
+    
+    if (nameValue) {
+    placeholderElement.innerText = nameValue + kmValue + ageValue;
+    }
+});
+
+buttonDeleteElement.addEventListener('click', function () {
+    nameElement.value = '';
+    kmElement.value = '';
+    ageElement.value = '';
+});
+
+// Calcolo il prezzo del biglietto
+if (userAge < 18) { 
+    percentage = 20;
+    discountValue = (sum / 100) * percentage;
+    console.log(discountValue);
+} else if (userAge >= 65) {
+    percentage = 40;
+    discountValue = (sum / 100) * percentage;
+    console.log(discountValue);
+} else {
+    console.log(sum);
+}
+
+// finalPrice = (sum - discountValue).toFixed(2);
+// console.log(`${message} ${finalPrice} €`);
